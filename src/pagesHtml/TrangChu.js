ef5d98212a4eab89ec22542ea607b663014a67e2
Doan Van Hoang <hoangusa08@gmail.com>
2020-12-15 14:17:55 +0700

    set title

--- src/pagesHtml/TrangChu.js
+++ src/pagesHtml/TrangChu.js
@@ -14,11 +14,7 @@ export default function MainPage() {
         });
     }, []);
     return (
-        
         <div>
-            <Helmet>
-             <title>{ TITLE }</title>
-            </Helmet>
             <div className="bar">
                 <div className="Link">
                     <h2>TRƯỜNG THPT PHẠM VĂN ĐỒNG</h2>                    
