1a5fb46407305af29de1c5e15604d531d06599e4
Doan Van Hoang <hoangusa08@gmail.com>
2020-12-26 15:08:37 +0700

    cái đuôi

--- src/pagesHtml/TrangChu.js
+++ src/pagesHtml/TrangChu.js
@@ -17,7 +17,7 @@ export default function MainPage() {
         <div>
             <div className="bar">
                 <div className="Link">
-                    <h2>TRƯỜNG THPT PHẠM VĂN ĐỒNG</h2>                    
+                    <h2>TRƯỜNG THPT PHẠM VĂN ĐỒNG</h2>
                     <div id='barright'>
                         <a href="https://www.facebook.com/thptphamvandong">fanpages</a>
                         <a href="/login">Đăng Nhập</a>
@@ -37,20 +37,20 @@ export default function MainPage() {
                     })}
                 </div>
             </div>
-            <div className = 'tail_f'>
-            <div className='tail'>
-                <p className='tail_tieude'>
-                    <center> CƠ QUAN CHỦ QUẢN </center> <br></br>
+            <div className='tail_f'>
+                <div className='tail'>
+                    <p className='tail_tieude'>
+                        <center> CƠ QUAN CHỦ QUẢN </center> <br></br>
                     TRƯỜNG THPT PHẠM VĂN ĐỒNG (PVĐ)
-                </p>                
-                <p className='tail_noidung'>
-                    <b>Địa chỉ:</b> TT. Mộ Đức, Mộ Đức, Quảng Ngãi <br></br>
-                    <b>Điện thoại:</b> +84-12-3456789 <br></br>
-                    <b>Email:</b> thpttrankimdong@quangngai.edu.vn<br></br>
-                    <b>Website:</b> thpttrankimdong.edu.vn
-                </p>                
-            </div>
-            <div className='mother_of_tail'>
+                </p>
+                    <p className='tail_noidung'>
+                        <b>Địa chỉ:</b> TT. Mộ Đức, Mộ Đức, Quảng Ngãi <br></br>
+                        <b>Điện thoại:</b> +84-12-3456789 <br></br>
+                        <b>Email:</b> thpttrankimdong@quangngai.edu.vn<br></br>
+                        <b>Website:</b> thpttrankimdong.edu.vn
+                </p>
+                </div>
+                <div className='mother_of_tail'>
                     © 2020 - Trường THPT Phạm Văn Đồng - Quảng Ngãi
             </div>
             </div>
